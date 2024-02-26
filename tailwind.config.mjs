8079f3c1c6f58794af889793ff8ded9b04a51d42
maximoaparicio <maxcodingg@gmail.com>
2024-02-26 13:08:08 -0300

    huge changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,18 @@
+import animations from "@midudev/tailwind-animations";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-    colors: {
-      aqua: "#00FFFF",
-    },
-  },
-  plugins: [],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#1f1f1f",
+				secondary: "#ddd",
+				aqua: "#7acded",
+				aqua2: "#2d6ca0",
+			},
+		},
+	},
+	darkMode: "class",
+	plugins: [animations],
 };
