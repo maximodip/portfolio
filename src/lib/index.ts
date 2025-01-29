40044d1f715b35cfd162948438f15377f7153063
maximodip <maxcodingg@gmail.com>
2025-01-29 16:13:07 -0300

    add latest projects

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,11 @@ import React from "@/icons/React.astro";
 import MySql from "@/icons/MySql.astro";
 import Express from "@/icons/Express.astro";
 import Boostrap from "@/icons/Bootstrap.astro";
+import Astro from "@/icons/AstroIcon.astro";
+import JavascriptIcon from "@/icons/JavascriptIcon.astro";
+import Nextjs from "@/icons/Nextjs.astro";
+import Prisma from "@/icons/Prisma.astro";
+import Shadcn from "@/icons/Shadcn.astro";
 
 export const navItems = [
   {
@@ -28,6 +33,36 @@ export const navItems = [
 ];
 
 const TAGS = {
+  SHADCN: {
+    name: "Shadcn",
+    class:
+      "dark:bg-[#23272f] dark:text-white duration-200 border border-[#1f1f1f]",
+    icon: Shadcn,
+  },
+  PRISMA: {
+    name: "Prisma",
+    class:
+      "dark:bg-[#23272f] dark:text-white duration-200 border border-[#1f1f1f]",
+    icon: Prisma,
+  },
+  NEXTJS: {
+    name: "Next.js",
+    class:
+      "dark:bg-[#000000] dark:text-white duration-200 border border-[#1f1f1f]",
+    icon: Nextjs,
+  },
+  JAVASCRIPT: {
+    name: "JavaScript",
+    class:
+      "dark:bg-[#23272f] dark:text-white duration-200 border border-[#1f1f1f]",
+    icon: JavascriptIcon,
+  },
+  ASTRO: {
+    name: "Astro",
+    class:
+      "dark:bg-[#23272f] dark:text-white duration-200 border border-[#1f1f1f]",
+    icon: Astro,
+  },
   TAILWIND: {
     name: "Tailwind",
     class:
@@ -61,6 +96,23 @@ const TAGS = {
 };
 
 export const PROJECTS = [
+  {
+    title: "Marcelo Muebles",
+    description:
+      "Fornitures Showroom for clients who wants to see the work done.",
+    github: "https://github.com/maximodip/marcelo-muebles",
+    image: "/projects/marcelo-muebles.jpg",
+    tags: [TAGS.ASTRO, TAGS.TAILWIND, TAGS.JAVASCRIPT],
+    link: "https://marceloaparicio.com",
+  },
+  {
+    title: "Cuentas Farmacia",
+    description:
+      "A web app to manage the accounts of a pharmacy. It has the products with price and a dashboard with dates to manage the accounts.",
+    github: "https://github.com/maximodip/prisma-farmacia",
+    image: "/projects/cuentas-sisa.png",
+    tags: [TAGS.NEXTJS, TAGS.PRISMA, TAGS.SHADCN, TAGS.MYSQL],
+  },
   {
     title: "Stock App",
     description:
